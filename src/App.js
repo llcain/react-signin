2df6c9636a246f98e845c1ec62c3cb55a30c7f35
llcain <llcain2012@gmail.com>
2023-04-25 20:32:12 -0500

    Created the HTML of the sign in form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+      <h2 className='signin-title'>Sign in</h2>
+      <form className='signin-form'>
+        <div>
+          <label for="name">First Name</label>
+          <input
+            id='name'
+            type='text'
+            placeholder='Bill'
+            name='name'
+            required
+          />
+        </div>
+
+        <div>
+          <label for="name">Last Name</label>
+          <input
+            id='name'
+            type='text'
+            placeholder='Buruk'
+            name='name'
+            required
+          />
+        </div>
+
+        <div>
+          <label for="email">Email</label>
+          <input
+            id='email'
+            type='email'
+            placeholder='me@example.com'
+            name='email'
+            required
+          />
+        </div>
+
+        <div>
+          <label for="password">Password</label>
+          <input
+            id='password'
+            type='password'
+            placeholder='password'
+            name='password'
+            required
+          />
+        </div>
+
+        <button className='btn btn--form' type='submit' value="Log in">Sign in</button>
+      </form>
     </div>
+    </>
+    
+    
   );
 }
 
